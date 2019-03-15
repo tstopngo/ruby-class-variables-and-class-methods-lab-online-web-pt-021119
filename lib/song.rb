c2b7aab667a93bf2d0a84e7f112bb8b84c0f5154
Tracy Ngo <tngo1206@gmail.com>
2019-03-15 01:28:54 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+  @@song_count = 0
+  @@song_artists = []
+  @@song_genres = []
+  @@song_genre_count = {}
+  @@song_artist_count = {}
+    
+  attr_accessor :name, :artist, :genre
+  
+  def intialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@song_count += 1
+    @@song_artists <<  @artist
+    @@song_genres << @genre
+    
+  end
+  
+  def self.count
+    @@song_count
+  end
+  
+  def self.artists
+    @@song_artists & @@song_artists
+  end
+  
+  def self.genres
+    @@song_genres & @@song_genres
+  end
+  
+  def self.genre_count
+    @@song_genre_count
+  end
+  
+    def self.song_count
+    @@song_artist_count
+  end
+end
